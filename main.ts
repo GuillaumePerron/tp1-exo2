5d64a13d5445c68685e10f4f809f6be7a45f0e56
GuillaumePerron <67984495+GuillaumePerron@users.noreply.github.com>
2020-07-27 08:06:34 +1100

    Fonctions et tableaux

--- main.ts
+++ main.ts
@@ -1,73 +1,33 @@
+function Aller_retour (x: number) {
+    while (liste[x] < 4) {
+        led.unplot(x, liste[x])
+        liste[x] = liste[x] + 1
+        led.plot(x, liste[x])
+        basic.pause(100)
+    }
+    while (liste[x] > 0) {
+        led.unplot(x, liste[x])
+        liste[x] = liste[x] - 1
+        led.plot(x, liste[x])
+        basic.pause(100)
+    }
+}
+let liste: number[] = []
 let y = 0
-let liste = [0, 1, 2, 3, 4]
+liste = [0, 1, 2, 3, 4]
 led.plot(0, 0)
 basic.forever(function () {
-    while (liste[1] < 4) {
-        led.unplot(1, liste[1])
-        liste[1] = liste[1] + 1
-        led.plot(1, liste[1])
-        basic.pause(200)
-    }
-    while (liste[1] > 0) {
-        led.unplot(1, liste[1])
-        liste[1] = liste[1] - 1
-        led.plot(1, liste[1])
-        basic.pause(200)
-    }
+    Aller_retour(0)
 })
 basic.forever(function () {
-    while (liste[0] < 4) {
-        led.unplot(0, liste[0])
-        liste[0] = liste[0] + 1
-        led.plot(0, liste[0])
-        basic.pause(200)
-    }
-    while (liste[0] > 0) {
-        led.unplot(0, liste[0])
-        liste[0] = liste[0] - 1
-        led.plot(0, liste[0])
-        basic.pause(200)
-    }
+    Aller_retour(1)
 })
 basic.forever(function () {
-    while (liste[3] < 4) {
-        led.unplot(3, liste[3])
-        liste[3] = liste[3] + 1
-        led.plot(3, liste[3])
-        basic.pause(200)
-    }
-    while (liste[3] > 0) {
-        led.unplot(3, liste[3])
-        liste[3] = liste[3] - 1
-        led.plot(3, liste[3])
-        basic.pause(200)
-    }
+    Aller_retour(2)
 })
 basic.forever(function () {
-    while (liste[4] < 4) {
-        led.unplot(4, liste[4])
-        liste[4] = liste[4] + 1
-        led.plot(4, liste[4])
-        basic.pause(200)
-    }
-    while (liste[4] > 0) {
-        led.unplot(4, liste[4])
-        liste[4] = liste[4] - 1
-        led.plot(4, liste[4])
-        basic.pause(200)
-    }
+    Aller_retour(3)
 })
 basic.forever(function () {
-    while (liste[2] < 4) {
-        led.unplot(2, liste[2])
-        liste[2] = liste[2] + 1
-        led.plot(2, liste[2])
-        basic.pause(200)
-    }
-    while (liste[2] > 0) {
-        led.unplot(2, liste[2])
-        liste[2] = liste[2] - 1
-        led.plot(2, liste[2])
-        basic.pause(200)
-    }
+    Aller_retour(4)
 })
